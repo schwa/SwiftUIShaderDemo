2c220e4a4320ba3d0ac3d407b9b94db79b7da57a
Jonathan Wight <jwight@mac.com>
2023-06-16 13:20:48 -0700

    Fixed DOH moment in code.

--- ShaderTest/ContentView.swift
+++ ShaderTest/ContentView.swift
@@ -4,21 +4,32 @@ import simd
 struct ContentView: View {
 
     let function = ShaderLibrary.myShader
-//    let function = ShaderLibrary.gameOfLife
+    //    let function = ShaderLibrary.gameOfLife
+
+    @State
+    var start = Date.now
 
     var body: some View {
-        VStack {
-            TimelineView(.animation) { context in
-                //Text("\(context.date.timeIntervalSince1970)").monospaced()
-                Color.black
-                .layerEffect(shader(context.date.timeIntervalSince1970), maxSampleOffset: .zero)
-                .frame(width: 256, height: 256)
-                Text("\(context.date.timeIntervalSince1970)").monospaced()
+        TimelineView(.animation) { timeline in
+            let time = Float(timeline.date.timeIntervalSince(start)) * 20
+            //let t = now.remainder(dividingBy: 5) * 20
+
+            VStack {
+                HStack {
+                    Color.black
+                        .layerEffect(shader(time), maxSampleOffset: .zero)
+                        .frame(width: 256, height: 256)
+
+                    Image(systemName: "iphone.gen1.radiowaves.left.and.right").font(.title).scaleEffect(CGSize(width: 6, height: 6))
+                        .layerEffect(shader(time), maxSampleOffset: .zero)
+                        .frame(width: 256, height: 256)
+                }
+                Text("\(time)")
             }
         }
     }
 
-    func shader(_ time: CFTimeInterval) -> Shader {
+    func shader(_ time: Float) -> Shader {
         return Shader(function: function, arguments: [.float2(256, 256), .float(time)])
     }
 }
